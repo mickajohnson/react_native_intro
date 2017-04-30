2f310ede93f4bf112034288d3f6323238b52ec48
Mick Johnson <mickajohnson@outlook.com>
2017-04-30 00:05:44 -0500

    first commit

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+// Import a library to help render component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// create a component
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Header headerText="Albums" />
+      <AlbumList />
+    </View>
+  );
+};
+
+
+// Render it to device
+AppRegistry.registerComponent('albums', () => App);
